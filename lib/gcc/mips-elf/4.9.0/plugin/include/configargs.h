ea3ecdcfea17f139834b139b4251fd164ceec87d
Vuong Nguyen <vuongdnguyen@hotmail.com>
2020-12-27 02:25:36 -0500

    GNU MIPS compiler for MCORE

---
+++ lib/gcc/mips-elf/4.9.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --target=mips-elf --prefix=/home/vuong/mips-elf --without-headers --with-newlib --with-gnu-as --with-gnu-ld";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
